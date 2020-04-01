acc82ece4144bd1679e6342286bd2473ff1ebe8c
Andrea Bagnacani <andrea.bagnacani@uni-rostock.de>
2020-04-01 10:53:32 +0200

    added prefixes

--- generator.js
+++ generator.js
@@ -29,7 +29,27 @@ function generator() {
     'vetero',
     'neo',
     'demono',
-    'corona'
+    'corona',
+    'feudo',
+    'omni',
+    'esa',
+    'penta',
+    'tetra',
+    'novo',
+    'contra',
+    'infra',
+    'intra',
+    'avio',
+    'trans',
+    'onico',
+    'gastro',
+    'pneumo',
+    'orto',
+    'alea',
+    'supra',
+    'alveo',
+    'non'
+
   ]
 
   var suffixes = [
